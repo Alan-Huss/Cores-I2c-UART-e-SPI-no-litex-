c8ad21007de37903b572429dd7ac91fe9a7a3590
Alan Lopes <alanhuss@ymail.com>
2025-10-28 21:03:34 -0300

    commit inicial com tudo rodando

---
+++ litex/colorlight_i5.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2021 Kazumoto Kojima <kkojima@rr.iij4u.or.jp>
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Arquivo "enxugado" para focar em SPI (LoRa) e I2C (AHT10)
+
+from migen import *
+
+from litex.gen import *
+
+from litex.build.io import DDROutput
+
+from litex_boards.platforms import colorlight_i5
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+# Imports necessários para o projeto
+from litex.soc.cores.spi import SPIMaster
+from litex.soc.cores.bitbang import I2CMaster
+from litex.soc.cores.gpio import GPIOOut
+from litex.build.generic_platform import Subsignal, Pins, IOStandard
+
+from litedram.modules import M12L64322A # Compatible with EM638325-6H.
+from litedram.phy import GENSDRPHY, HalfRateGENSDRPHY
+
+# CRG (Clock and Reset Generation) -----------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq, sdram_rate="1:1"):
+        self.rst    = Signal()
+        self.cd_sys = ClockDomain()
+        if sdram_rate == "1:2":
+            self.cd_sys2x    = ClockDomain()
+            self.cd_sys2x_ps = ClockDomain()
+        else:
+            self.cd_sys_ps = ClockDomain()
+
+        # # #
+
+        # Clk / Rst
+        # Usa o oscilador externo de 25MHz
+        clk = platform.request("clk25")
+        clk_freq = 25e6
+
+        rst_n = platform.request("cpu_reset_n")
+
+        # PLL Principal
+        self.pll = pll = ECP5PLL()
+        self.comb += pll.reset.eq(~rst_n | self.rst)
+        pll.register_clkin(clk, clk_freq)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        if sdram_rate == "1:2":
+            pll.create_clkout(self.cd_sys2x,    2*sys_clk_freq)
+            pll.create_clkout(self.cd_sys2x_ps, 2*sys_clk_freq, phase=180)
+        else:
+           pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=180)
+
+        # SDRAM clock
+        sdram_clk = ClockSignal("sys2x_ps" if sdram_rate == "1:2" else "sys_ps")
+        self.specials += DDROutput(1, 0, platform.request("sdram_clock"), sdram_clk)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, board="i5", revision="7.0", toolchain="trellis", sys_clk_freq=60e6,
+        sdram_rate             = "1:1",
+        with_led_chaser        = True,
+        **kwargs):
+        board = board.lower()
+        assert board in ["i5", "i9"]
+        platform = colorlight_i5.Platform(board=board, revision=revision, toolchain=toolchain)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq,
+            sdram_rate       = sdram_rate
+        )
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, int(sys_clk_freq), ident = "LiteX SoC on Colorlight " + board.upper(), **kwargs)
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            ledn = platform.request_all("user_led_n")
+            self.leds = LedChaser(pads=ledn, sys_clk_freq=sys_clk_freq)
+
+        # SPI Flash --------------------------------------------------------------------------------
+        if board == "i5":
+            from litespi.modules import GD25Q16 as SpiFlashModule
+        if board == "i9":
+            from litespi.modules import W25Q64 as SpiFlashModule
+
+        from litespi.opcodes import SpiNorFlashOpCodes as Codes
+        self.add_spi_flash(mode="1x", module=SpiFlashModule(Codes.READ_1_1_1))
+
+        # SDR SDRAM --------------------------------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            sdrphy_cls = HalfRateGENSDRPHY if sdram_rate == "1:2" else GENSDRPHY
+            self.sdrphy = sdrphy_cls(platform.request("sdram"))
+            self.add_sdram("sdram",
+                phy           = self.sdrphy,
+                module        = M12L64322A(sys_clk_freq, sdram_rate),
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+
+        # Configuração dos pinos SPI (para LoRa RFM95) -----------------------------------------------
+        spi_pads = [
+            ("spi", 0,
+                Subsignal("clk",  Pins("G20")),
+                Subsignal("mosi", Pins("L18")),
+                Subsignal("miso", Pins("M18")),
+                Subsignal("cs_n", Pins("N17")),
+                IOStandard("LVCMOS33")
+            ),
+            # RESET separado como GPIO
+            ("lora_reset", 0, Pins("L20"), IOStandard("LVCMOS33"))
+        ]
+
+        platform.add_extension(spi_pads)
+
+        # Adiciona o Core SPI Master e o CSR 'spi'
+        self.spi = SPIMaster(pads=platform.request("spi"), data_width=8, sys_clk_freq=sys_clk_freq, spi_clk_freq=1e6)
+        self.add_csr("spi")
+
+        # Adiciona o Core GPIOOut e o CSR 'lora_reset'
+        self.submodules.lora_reset = GPIOOut(platform.request("lora_reset"))
+        self.add_csr("lora_reset")
+
+        # Configuração dos pinos I2C (para AHT10) ---------------------------------------------------
+        i2c_pads = [
+            ("i2c", 0,
+                Subsignal("scl", Pins("U17")),
+                Subsignal("sda", Pins("U18")),
+                IOStandard("LVCMOS33")
+            )
+        ]
+
+        platform.add_extension(i2c_pads)
+        
+        # Adiciona o Core I2CMaster (Bitbang) e o CSR 'i2c'
+        self.submodules.i2c = I2CMaster(pads=platform.request("i2c"))
+        self.add_csr("i2c")
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=colorlight_i5.Platform, description="LiteX SoC on Colorlight I5.")
+    parser.add_target_argument("--board",            default="i5",             help="Board type (i5).")
+    parser.add_target_argument("--revision",         default="7.0",            help="Board revision (7.0).")
+    parser.add_target_argument("--sys-clk-freq",     default=60e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--sdram-rate",       default="1:1",            help="SDRAM Rate (1:1 Full Rate or 1:2 Half Rate).")
+    
+    
+    args = parser.parse_args()
+
+    soc = BaseSoC(board=args.board, revision=args.revision,
+        toolchain              = args.toolchain,
+        sys_clk_freq           = args.sys_clk_freq,
+        sdram_rate             = args.sdram_rate,
+        **parser.soc_argdict
+    )
+
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
